65ec10d0e4b4770eaccadb74a147aee051f5ad6d
SeppPenner <HansM2013@gmx.de>
2022-02-17 19:24:13 +0100

    Smaller changes.

--- src/SignalRForWPF.Client/GlobalUsings.cs
+++ src/SignalRForWPF.Client/GlobalUsings.cs
@@ -1,4 +1,4 @@
-#pragma warning disable IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
 global using System.ComponentModel;
 global using System.Globalization;
 global using System.Runtime.CompilerServices;
@@ -9,4 +9,4 @@ global using System.Windows;
 global using Microsoft.AspNetCore.SignalR.Client;
 
 global using SignalRForWPF.Shared;
-#pragma warning restore IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
